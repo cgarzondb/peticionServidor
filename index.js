1b87f8b7a6e20e43982061489994da2d77fae44e
cgarzondb <cgarzon@db-system.com>
2019-11-25 08:38:57 -0500

    acualizacoin

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 var express = require('express');
 var app = express();
 var request = require('request-promise'),
-    ruta = "https://jsonplaceholder.typicode.com/users";
+    ruta = "https://192.168.200.25:4000";
 
-request({
-    uri:ruta,
-    json:true,
-}).then(saludo =>{
+var aux = false;
 
-    console.log(saludo);
-    return "como le va";
 
+app.get('/saludo', function (req, res) {
+    aux = true;
+    res.send('jao');
 });
 
+if(aux){
+    request({
+        uri:ruta,
+        json:true,
+    }).then(saludo =>{
+
+        if(saludo !="" || null){
+            console.log(saludo);
+            aux = false;
+        }
+    });
+}
 
 app.listen(3000, function () {
  console.log('Example app listening on port 3000!');
